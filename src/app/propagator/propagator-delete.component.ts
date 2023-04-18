33234885995afa5c4ddcdaca9029c3aeb0621135
patrickpelegrina1 <patrickpelegrinacuen@gmail.com>
2023-04-18 18:10:13 +0200

    imported propagator-delete.component.ts

---
+++ src/app/propagator/propagator-delete.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
+import { UserService } from '../user.service';
+import { User } from '../../login-basic/user';
+
