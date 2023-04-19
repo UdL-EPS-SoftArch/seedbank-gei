1855efa4237ffa4f0b3359dba32b89d9a34e3c72
MarioMontanuy <mariobnf58@gmail.com>
2023-04-19 17:25:31 +0200

    Adds initial Take implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,12 +23,14 @@ import {HttpErrorInterceptor} from './error-handler/http-error-interceptor';
 import {AuthenticationBasicService} from './login-basic/authentication-basic.service';
 import {LoggedInGuard} from './login-basic/loggedin.guard';
 import {UserService} from './user/user.service';
+import {TakeComponent} from "./take/take.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     AboutComponent,
+    TakeComponent,
     NotFoundComponent,
     UserListComponent,
     UserDetailComponent,
