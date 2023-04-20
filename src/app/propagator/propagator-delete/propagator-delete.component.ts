99a31a07c8fe31d5eb8a636b3fe8acb8556d7ae9
patrickpelegrina1 <patrickpelegrinacuen@gmail.com>
2023-04-20 15:48:55 +0200

    propagator components ng
                                                                                                                  Co-authored-by: Joel Guitart<joelguitartcarvajal@gmail.com>, Patrick Pelegrina<patrickpelegrinacuen@gmail.com>

---
+++ src/app/propagator/propagator-delete/propagator-delete.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
+import { PropagatorService } from '../propagator.service';
+import { Propagator } from '../../login-basic/propagator';
+
+@Component({
+  selector: 'app-propagator-delete',
+  templateUrl: './propagator-delete.component.html',
+  styleUrls: ['./propagator-delete.component.css']
+})
+export class PropagatorDeleteComponent implements OnInit {
+  public propagator: Propagator = new Propagator();
+    private id: string;
+
+    constructor(private route: ActivatedRoute,
+                  private router: Router,
+                  private propagatorService: PropagatorService,
+                  private authenticationService: AuthenticationBasicService) {
+      }
+
+    ngOnInit(): void {
+      this.id = this.route.snapshot.paramMap.get('id');
+      this.propagatorService.getResource(this.id).subscribe(
+        propagator => this.propagator = propagator);
+    }
+
+    delete(): void {
+      this.propagatorService.deleteResource(this.propagator).subscribe(
+        () => {
+          this.authenticationService.logout();
+          this.router.navigate(['']);
+        });
+    }
+}
