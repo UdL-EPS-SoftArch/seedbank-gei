f69b9001f30a1e7657a35965623b300fdb0fa5ee
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-04-20 16:57:51 +0200

    feat: get all entities from donation screen
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DonationService } from '../donation.service';
+import { Donation } from './donation';
+import { Donor } from '../donor';
+import { Take } from '../take';
+import { Propagator } from '../propagator';
 
 @Component({
   selector: 'app-donation',
@@ -9,14 +14,30 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class DonationComponent implements OnInit {
 
   id: string | null = null;
+  donation: Donation | null = null;
 
   constructor(
     public router: Router,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private donationService: DonationService
+    ) {
   }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
+    this.donationService.getResource(this.id).subscribe(donation => {
+      console.log(donation);
+      this.donation = donation;
+      this.donation.getRelation<Donor>("donor").subscribe(donor => {
+        console.log(`Donor: ${JSON.stringify(donor)}`);
+      });
+      this.donation.getRelation<Take>("takenBy").subscribe(takenBy => {
+        console.log(`TakenBy: ${JSON.stringify(takenBy)}`);
+        takenBy.getRelation<Propagator>("takePropagator").subscribe(propagator => {
+          console.log(`Propagator: ${JSON.stringify(propagator)}`);
+        })
+      })
+    });
   }
 
 }
