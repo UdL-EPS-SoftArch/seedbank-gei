31fc58efb3e008c2b7375b5ad39d4e6d0f56ebdc
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-04-20 18:11:43 +0200

    fix: donation `donor` type
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/app/donation/donation.ts
+++ src/app/donation/donation.ts
@@ -1,9 +1,10 @@
 import { HateoasResource, Resource } from '@lagoshny/ngx-hateoas-client';
 import { Batch } from '../batch';
 import { Take } from '../take';
+import { Donor } from '../donor';
 
 @HateoasResource('donations')
 export class Donation extends Batch {
-  donor: Donation
+  donor: Donor
   takeBy: Take
 }
