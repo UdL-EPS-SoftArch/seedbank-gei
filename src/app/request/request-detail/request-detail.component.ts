f77d90c031e080cf250a66dd1560470caaf7cec4
Oriol Agost Batalla <gpg18@agost.info>
2023-04-20 19:59:14 +0200

    feat: implements request details component
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/app/request/request-detail/request-detail.component.ts
+++ src/app/request/request-detail/request-detail.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Request} from "../request";
+import {ActivatedRoute} from "@angular/router";
+import {RequestService} from "../request.service";
+import {Propagator} from "../../propagator";
+import {Take} from "../../take";
 
 @Component({
   selector: 'app-request-detail',
   templateUrl: './request-detail.component.html',
   styleUrls: ['./request-detail.component.css']
 })
-export class RequestDetailComponent {
+export class RequestDetailComponent implements OnInit {
+  public request: Request = new Request();
 
+  constructor(
+    private route: ActivatedRoute,
+    private requestService: RequestService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.requestService.getResource(id).subscribe(
+      request => {
+        this.request = request
+        console.log(this.request)
+        this.request.getRelation<Propagator>('propagator').subscribe(propagator => {
+            console.log(`Propagator: ${JSON.stringify(propagator)}`)
+          }
+        )
+        this.request.getRelation<Take>('take').subscribe(take => {
+            console.log(`Take: ${JSON.stringify(take)}`)
+          }
+        )
+      },
+    )
+  }
 }
