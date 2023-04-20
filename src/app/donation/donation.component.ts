95c9ba794e9785c58aa2f3711667f674f5c55140
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-04-20 11:14:16 +0200

    feat: donation routing
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>

--- src/app/donation/donation.component.ts
+++ src/app/donation/donation.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-donation',
   templateUrl: './donation.component.html',
   styleUrls: ['./donation.component.css']
 })
-export class DonationComponent {
+export class DonationComponent implements OnInit {
+
+  id: string | null = null;
+
+  constructor(
+    public router: Router,
+    private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+  }
 
 }
