cc72a0c0363e4b4f7b12f0f3f1440e716245f17d
Oriol Agost Batalla <gpg18@agost.info>
2023-04-20 20:53:06 +0200

    feat: implements request detail
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/app/request/request-detail/request-detail.component.ts
+++ src/app/request/request-detail/request-detail.component.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Request} from "../request";
 import {ActivatedRoute} from "@angular/router";
 import {RequestService} from "../request.service";
+import {firstValueFrom} from "rxjs";
 import {Propagator} from "../../propagator";
-import {Take} from "../../take";
 
 @Component({
   selector: 'app-request-detail',
@@ -19,21 +19,9 @@ export class RequestDetailComponent implements OnInit {
   ) {
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     const id = this.route.snapshot.paramMap.get('id');
-    this.requestService.getResource(id).subscribe(
-      request => {
-        this.request = request
-        console.log(this.request)
-        this.request.getRelation<Propagator>('propagator').subscribe(propagator => {
-            console.log(`Propagator: ${JSON.stringify(propagator)}`)
-          }
-        )
-        this.request.getRelation<Take>('take').subscribe(take => {
-            console.log(`Take: ${JSON.stringify(take)}`)
-          }
-        )
-      },
-    )
+    this.request = await firstValueFrom(this.requestService.getResource(id));
+    this.request.propagator = await firstValueFrom(this.request.getRelation<Propagator>('propagator'));
   }
 }
