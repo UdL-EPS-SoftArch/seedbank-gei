0916e2ce2d1221b532aa16e3506298f42a70c551
sys-walker <pere.rollon@gmail.com>
2023-04-20 11:01:58 +0200

    Add TakeList request
    Co-authored-by: MarioMontanuy <mariobnf58@gmail.com>
    Co-authored-by: Oscars35 <oscar13785@gmail.com>

--- src/app/take/take-list/take-list.component.ts
+++ src/app/take/take-list/take-list.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TakeService } from '../take-service';
+import { PagedResourceCollection } from '@lagoshny/ngx-hateoas-client';
+import { Take } from '../take-model';
 @Component({
   selector: 'app-take-list',
   templateUrl: './take-list.component.html',
   styleUrls: ['./take-list.component.css']
 })
-export class TakeListComponent {
-
+export class TakeListComponent implements OnInit {
+  public pageSize = 5;
+  public page = 1;
+  public totalTakes = 0;
+  takes:any[]=[]
+  constructor(public router: Router, public takeService:TakeService){
+  }
+  detail(take: any): void {
+    this.router.navigate(['take', take.id]);
+  }
+  ngOnInit(){
+    this.takeService.getPage({ pageParams:  { size: this.pageSize }, sort: { username: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<Take>) => {
+        this.takes = page.resources;
+        console.log(page);
+        
+        this.totalTakes = page.totalElements;
+      });
+  }
 }
