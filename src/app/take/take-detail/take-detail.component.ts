a006e60665d8054fa7553c474e21e8459c16052f
sys-walker <pere.rollon@gmail.com>
2023-04-25 18:09:14 +0200

    Add role checking in take CRUD operations
    Co-authored-by: MarioMontanuy <mariobnf58@gmail.com>
    Co-authored-by: Oscars35 <oscar13785@gmail.com>

--- src/app/take/take-detail/take-detail.component.ts
+++ src/app/take/take-detail/take-detail.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Take } from '../take-model';
 import { TakeService } from '../take-service';
+import { AuthenticationBasicService } from 'src/app/login-basic/authentication-basic.service';
 
 @Component({
   selector: 'app-take-detail',
@@ -10,12 +11,15 @@ import { TakeService } from '../take-service';
 })
 export class TakeDetailComponent {
   takeDetail:Take
-  constructor(private route: ActivatedRoute,private takeService: TakeService) {
+  constructor(private route: ActivatedRoute,private takeService: TakeService,private authenticationService: AuthenticationBasicService) {
   }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.takeService.getResource(id).subscribe(take => { this.takeDetail = take;});
+  }
+  isRole(role: string): boolean {
+    return this.authenticationService.isRole(role);
   }
   
 }
