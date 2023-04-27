6b132ab4d003a3d6c4813e28c1beef8be4f77efd
Oriol Agost Batalla <gpg18@agost.info>
2023-04-27 21:45:28 +0200

    feat: implement skeleton of request list componenent
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/app/request/request-list/request-list.component.ts
+++ src/app/request/request-list/request-list.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {RequestService} from "../request.service";
+import {PagedGetOption, PagedResourceCollection, SortOrder} from "@lagoshny/ngx-hateoas-client";
+import {switchMap} from "rxjs/operators";
+import {Request} from "../request";
+import {Propagator} from "../../propagator";
+import {Take} from "../../take";
 
 @Component({
   selector: 'app-request-list',
   templateUrl: './request-list.component.html',
   styleUrls: ['./request-list.component.css']
 })
-export class RequestListComponent {
+export class RequestListComponent implements OnInit {
+  public requests: Request[] = []
 
+  constructor(
+    private route: ActivatedRoute,
+    private requestService: RequestService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.requestService.getPage(this.getPageOptions()).subscribe((page: PagedResourceCollection<Request>) => {
+        this.requests = page.resources
+        // const totalElements = page.page.totalElements
+        // const pagedResources = page
+        // this.requests.map((request: Request) => {
+        //
+        // })
+        this.requests.map((request: Request) => {
+          request.getRelation<Propagator>('propagator').pipe(
+              switchMap((propagator: Propagator) => {
+                request.propagator = propagator
+                return request.getRelation<Take>('fulFilledBy')
+              }),
+          ).subscribe((fulfilledBy: Take) => request.fulfilledBy = fulfilledBy)
+          })
+      }
+    )
+  }
+
+  private getPageOptions(): PagedGetOption {
+    const pageSize = 10
+    const sortOrder: 'ASC' | 'DESC' = 'ASC'
+    return {pageParams: {size: pageSize}, sort: {username: sortOrder}}
+  }
 }
