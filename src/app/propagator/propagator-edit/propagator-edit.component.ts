de5f86a0db49a4010d209226ac2d5d3bc62bbd3c
JoelGuitartCarvajal <joelguitartcarvajal@gmail.com>
2023-05-03 17:56:08 +0200

    added patrick components

--- src/app/propagator/propagator-edit/propagator-edit.component.ts
+++ src/app/propagator/propagator-edit/propagator-edit.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthenticationBasicService } from '../../login-basic/authentication-basic.service';
+import { PropagatorService } from '../propagator.service';
+import { Propagator } from '../propagator';
+
 
 @Component({
   selector: 'app-propagator-edit',
@@ -6,5 +12,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./propagator-edit.component.css']
 })
 export class PropagatorEditComponent {
+  public propagator: Propagator = new Propagator();
 
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private propagatorService: PropagatorService,
+              private authenticationService: AuthenticationBasicService) {
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.propagatorService.getResource(id).subscribe(
+      (propagator: Propagator) => this.propagator = propagator );
+  }
+
+  onSubmit(): void {
+      this.propagator.password = this.propagator.passwordReset ? this.propagator.password : undefined; // Don't edit if not a reset
+      this.propagatorService.patchResource(this.propagator).subscribe(
+        (patchedPropagator: Propagator) => {
+          if (this.propagator.passwordReset) {
+            this.authenticationService.logout();
+            this.authenticationService.login(this.propagator.id, this.propagator.password).subscribe(
+              (propagator: Propagator) => this.router.navigate(['propagators', propagator.id]));
+          } else {
+            this.router.navigate(['propagators', patchedPropagator.id]);
+          }
+        });
+    }
+
+  getCurrentUserName(): string {
+    return this.authenticationService.getCurrentUser().id;
+  }
 }
