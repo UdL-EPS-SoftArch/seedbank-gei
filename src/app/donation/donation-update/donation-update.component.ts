ed99facc65dd1ea0fb4ac0efaf800ef9440173bd
Oriol Agost Batalla <gpg18@agost.info>
2023-05-09 21:36:30 +0200

    feat: adds donation update component
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

---
+++ src/app/donation/donation-update/donation-update.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {Donation} from "../donation";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Location} from "@angular/common";
+import {AuthenticationBasicService} from "../../login-basic/authentication-basic.service";
+import {DonationService} from "../donation.service";
+
+@Component({
+  selector: 'app-donation-update',
+  templateUrl: './donation-update.component.html',
+  styleUrls: ['./donation-update.component.scss']
+})
+export class DonationUpdateComponent {
+  public donation: Donation;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private locationService: Location,
+    private donationService: DonationService
+  ) {
+  }
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.donationService.getResource(id).subscribe(
+      (donation: Donation) => this.donation = donation);
+  }
+
+  onSubmit(): void {
+    this.donationService.patchResource(this.donation).subscribe(
+      (modifiedDonation: Donation) => {
+        this.router.navigate(['donations', modifiedDonation.id]).then();
+      });
+  }
+
+  onCancel(): void {
+    this.locationService.back();
+  }
+}
