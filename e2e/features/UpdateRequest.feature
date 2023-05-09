8951dfd10104089a3bb2bc765c3500e9404ad560
FerranAD <ferranaran@tuta.io>
2023-05-09 20:12:52 +0200

    test(request): add successful scenario
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- e2e/features/UpdateRequest.feature
+++ e2e/features/UpdateRequest.feature
@@ -2,3 +2,24 @@ Feature: Create a request
   In order to use the app
   As a propagator
   I want to modify requests
+
+  Scenario: Update a request with valid data
+    Given I'm in the homepage logged in as a propagator and I create a request with
+      | FIELD    | VALUE         |
+      | amount   | 123           |
+      | weight   | 123           |
+      | location | Lleida        |
+    When I go to requests page and click on the last request
+    And I click on edit
+    And I fill the form with
+      | FIELD    | VALUE         |
+      | amount   | 456           |
+      | weight   | 456           |
+      | location | Barcelona     |
+    Then I am redirected to the request details page
+    And I should see the request with
+      | FIELD    | VALUE         |
+      | amount   | 456           |
+      | weight   | 456           |
+      | location | Barcelona     |
+
--- e2e/steps/update-request.ts
+++ e2e/steps/update-request.ts
@@ -1,2 +1,29 @@
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
-import { DataTable } from '@cucumber/cucumber';
+import {Given, When, Then} from 'cypress-cucumber-preprocessor/steps';
+import {DataTable} from '@cucumber/cucumber';
+
+Given(/^I'm in the homepage logged in as a propagator and I create a request with$/, function (table: DataTable) {
+  cy.visit('http://localhost:4200');
+  cy.get('.nav-link').contains('Login').click();
+  cy.get('#username').type('propagator1').blur();
+  cy.get('#password').type('password').blur();
+  cy.get('button').contains('Submit').click();
+  cy.get('.nav-link').contains('Requests').click();
+  cy.get('#createRequest').click();
+  table.rows().forEach((pair: string[]) =>
+    cy.get('#' + pair[0]).type(pair[1]).blur());
+});
+When(/^I go to requests page and click on the last request$/, function () {
+  cy.get('.nav-link').contains('Requests').click();
+  cy.get('.card-text').last().click();
+});
+When(/^I click on edit$/, function () {
+  cy.get('#editBtn').click()
+});
+Then(/^I should see the request with$/, function (table: DataTable) {
+  cy.get('td').eq(1).should('contain', table.rows()[0][1]);
+  cy.get('td').eq(3).should('contain', table.rows()[1][1]);
+  cy.get('td').eq(5).should('contain', table.rows()[2][1]);
+});
+Then(/^I should not see the edit button$/, function () {
+  cy.get('#editBtn').should('not.exist');
+});
