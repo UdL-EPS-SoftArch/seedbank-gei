e590447be42f89a00ae968c0b1e51caa1a59b78a
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-05-09 16:01:55 +0200

    feat: implement donation delete
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/app/donation/donation-delete/donation-delete.component.ts
+++ src/app/donation/donation-delete/donation-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {Donation} from "../donation";
+import {DonationService} from "../../donation.service";
 
 @Component({
   selector: 'app-donation-delete',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./donation-delete.component.scss']
 })
 export class DonationDeleteComponent {
+  public donation: Donation
+
+  constructor(
+    private route : ActivatedRoute,
+    private router: Router,
+    private donationService: DonationService,
+  ) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.donationService.getResource(id).subscribe((donation: Donation) => {
+      this.donation = donation;
+    });
+  }
+
+  delete() {
+    this.donationService.deleteResource(this.donation).subscribe(() => {
+      this.router.navigate(['/donations']).then();
+    });
+  }
 
 }
