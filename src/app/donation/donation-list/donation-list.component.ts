ce19c8b83dcf214e44a440e6242eac0eabe51122
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-05-09 10:21:46 +0200

    feat: fetch donations and number of donations
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/app/donation/donation-list/donation-list.component.ts
+++ src/app/donation/donation-list/donation-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PagedGetOption} from "@lagoshny/ngx-hateoas-client";
+import {DonationService} from "../../donation.service";
 
 @Component({
   selector: 'app-donation-list',
   templateUrl: './donation-list.component.html',
   styleUrls: ['./donation-list.component.scss']
 })
-export class DonationListComponent {
+export class DonationListComponent implements OnInit {
+
+  donations = []
+  numberDonations: number = 0;
+
+  constructor(private donationService: DonationService) { }
+
+  ngOnInit(): void {
+    const options: PagedGetOption = { pageParams: { size: 10 }, sort: { username: 'ASC' }};
+    this.donationService.getPage(options).subscribe((page) => {
+      this.donations = page.resources;
+      this.numberDonations = page.totalElements;
+      console.log(this.donations);
+      console.log(this.numberDonations);
+    });
+  }
 
 }
