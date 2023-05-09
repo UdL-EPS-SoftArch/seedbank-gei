dd3ce3eaeb153ea5d7b44a8325fe24cd3ac066cd
Oriol Agost Batalla <gpg18@agost.info>
2023-05-09 20:11:06 +0200

    feat: adds test for trying to delete a request without correct role
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- e2e/features/DeleteRequest.feature
+++ e2e/features/DeleteRequest.feature
@@ -12,3 +12,9 @@ Feature: Delete a request
     And I click on the delete button
     And I click on the confirm button
     Then I am redirected to the request list page
+
+  Scenario: Delete a request without being a propagator
+    Given I'm in the homepage logged in as a donor
+    When I click the requests dropdown
+    And I click the list button
+    Then I should not see the delete button
--- e2e/steps/delete-request.ts
+++ e2e/steps/delete-request.ts
@@ -1,5 +1,5 @@
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
-import { DataTable } from '@cucumber/cucumber';
+import {Given, When, Then} from 'cypress-cucumber-preprocessor/steps';
+import {DataTable} from '@cucumber/cucumber';
 
 When(/^I click the list button$/, function () {
   cy.get('#listRequests').click();
@@ -15,4 +15,7 @@ When(/^I click on the confirm button$/, function () {
 });
 Then(/^I am redirected to the request list page$/, function () {
   cy.url().should('match', /\/requests/);
+});
+Then(/^I should not see the delete button$/, function () {
+  cy.get('button').contains('Delete').should('not.exist');
 });
