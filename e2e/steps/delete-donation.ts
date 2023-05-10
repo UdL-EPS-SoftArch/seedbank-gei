af11adcdf2f94f25a60e5dc577acaaa5f770281e
Oriol Agost Batalla <gpg18@agost.info>
2023-05-10 14:54:49 +0200

    test(donation): adds delete donation steps
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

---
+++ e2e/steps/delete-donation.ts
@@ -0,0 +1,21 @@
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { DataTable } from '@cucumber/cucumber';
+
+When(/^I click the list button$/, function () {
+  cy.get('#listDonations').click();
+});
+When(/^I click on the first item$/, function () {
+  cy.get('.table').first().click();
+});
+When(/^I click on the delete button$/, function () {
+  cy.get('button').contains('Delete').click();
+});
+When(/^I click on the confirm button$/, function () {
+  cy.get('button').contains('Delete').click();
+});
+Then(/^I am redirected to the donation list page$/, function () {
+  cy.url().should('match', /\/donations/);
+});
+Then(/^I should not see the delete button$/, function () {
+  cy.get('button').contains('Delete').should('not.exist');
+});
