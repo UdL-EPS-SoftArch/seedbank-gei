a82871bdcb2f0e22cf24de00817455a1700cafce
sys-walker <pere.rollon@gmail.com>
2023-05-10 18:05:16 +0200

    Refactor typescript
    Co-authored-by: MarioMontanuy <mariobnf58@gmail.com>
    Co-authored-by: Oscars35 <oscar13785@gmail.com>
    Co-authored-by: sys-walker  <pere.rollon@gmail.com>

--- e2e/features/DeleteTake.feature
+++ e2e/features/DeleteTake.feature
@@ -1,4 +1,5 @@
 Feature: Delete Take
+
   Scenario: I Delete a new Take when logged in as admin
     Given I'm in the homepage
     And I log in as "admintest" with password "password"
@@ -37,11 +38,13 @@ Feature: Delete Take
     Then The delete button exist 
     And can click on delete
     Then I got redirected to take list
+
   Scenario: I try to delete a new Take when logged in as user but i cant acces to take list
     Given I'm in the homepage
     And I log in as "test" with password "password"
     Then I'm logged in as user "test"
     And I cant find the Take menu
+
   Scenario: I try to delete a new Take when logged in as donor but
     Given I'm in the homepage
     And I log in as "donor" with password "password"
--- e2e/steps/delete-take.ts
+++ e2e/steps/delete-take.ts
@@ -1,48 +1,50 @@
-import {And, Then} from "cypress-cucumber-preprocessor/steps";
+import { And, Then } from "cypress-cucumber-preprocessor/steps";
 
 And("I click the {string} dropdown menu", (option) => {
-  cy.get('.dropdown-item').contains(option).click();
-})
+  cy.get(".dropdown-item").contains(option).click();
+});
 
 Then("I'm in take list page", () => {
-  cy.visit('http://localhost:4200/take');
-  //cy.get('.card-block').click();
-})
-
-And('I click on the first item list', ()=>{
-  cy.get('.card-text').first().click()
-});
-Then('The delete button exist', ()=>{
-  cy.get('#deleteBtn').should('exist');
-});
-And('can click on delete', ()=>{
-  cy.get('#deleteBtn').should('exist');
-  cy.get('#deleteBtn').should('exist').click();
-});
-Then('I can see confirmation page', ()=>{
-  cy.get('#deleteBtn').should('exist');
-  cy.get('#listBtn').should('exist');
-});
-Then('I got redirected to take list',()=>{
-  cy.url().should("be.equals", 'http://localhost:4200/take')
-})
-And('I cant find the Take menu', () => {
-  cy.get('.nav-link').contains('Takes').should('not.exist');
-
+  cy.visit("http://localhost:4200/take");
 });
 
-And('I can only get back', () => {
-  cy.get('#listBtn').should('exist');
-  cy.get('#deleteBtn').should('not.exist');
-  cy.get('#editBtn').should('not.exist');
-  
+And("I click on the first item list", () => {
+  cy.get(".card-text").first().click();
 });
 
-And('I cancel confirmation',()=>{
-  cy.get('#listBtn').should('exist');
-  cy.get('#listBtn').should('exist').click();
-})
-Then('I got redirected to current take',()=>{
+Then("The delete button exist", () => {
+  cy.get("#deleteBtn").should("exist");
+});
 
-  cy.url().should("match", /http:\/\/localhost:4200\/take\/(\d+)/)
-})
+And("can click on delete", () => {
+  cy.get("#deleteBtn").should("exist");
+  cy.get("#deleteBtn").should("exist").click();
+});
+
+Then("I can see confirmation page", () => {
+  cy.get("#deleteBtn").should("exist");
+  cy.get("#listBtn").should("exist");
+});
+
+Then("I got redirected to take list", () => {
+  cy.url().should("be.equals", "http://localhost:4200/take");
+});
+
+And("I cant find the Take menu", () => {
+  cy.get(".nav-link").contains("Takes").should("not.exist");
+});
+
+And("I can only get back", () => {
+  cy.get("#listBtn").should("exist");
+  cy.get("#deleteBtn").should("not.exist");
+  cy.get("#editBtn").should("not.exist");
+});
+
+And("I cancel confirmation", () => {
+  cy.get("#listBtn").should("exist");
+  cy.get("#listBtn").should("exist").click();
+});
+
+Then("I got redirected to current take", () => {
+  cy.url().should("match", /http:\/\/localhost:4200\/take\/(\d+)/);
+});
