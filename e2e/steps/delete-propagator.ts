a6d3d16749c45ed5586cdd0c7fbdb66fd11d17ed
patrickpelegrina1 <patrickpelegrinacuen@gmail.com>
2023-05-12 20:32:05 +0200

    delete propagator fixed feature Co-authored-by: JoelGuitartCarvajal <joelguitartcarvajal@gmail.com>

--- e2e/steps/delete-propagator.ts
+++ e2e/steps/delete-propagator.ts
@@ -1,59 +1,29 @@
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps';
 import { DataTable } from '@cucumber/cucumber';
 
-Given('I\'m in the homepage', () => {
-  cy.visit('http://localhost:4200');
+Then('I see a confirmation message {string}', (message) => {
+  cy.log(message)
 });
 
-Given('I\'m not logged in', () => {
-  cy.get('.nav-link').contains('Login');
-});
+And("I click the {string} item", (option) => {
+  cy.visit('http://localhost:4200/users/user')
+})
 
-Given('I log in as {string} with password {string}', (username, password) => {
-  cy.get('.nav-link').contains('Login').click();
-  cy.get('#username').type(username).blur();
-  cy.get('#password').type(password).blur();
-  cy.get('button').contains('Submit').click();
-});
+And("I'm not logged in as propagator {string}", (option) => {
+    cy.get('.nav-link').contains('Login');
+    cy.log("Logged out propagator")
+}
 
-Given('I click the {string} menu', (option) => {
-  cy.get('.nav-link').contains(option).click();
-});
+And("The propagator is already selected"), () => {
+    cy.visit('http://localhost:4200/users/user')
+}
 
-When('I fill the form with', (table: DataTable) => {
-    table.rows().forEach((pair: string[]) =>
-      cy.get('#' + pair[0]).type(pair[1]).blur() );
- });
+Then("I'm in the Users list", () => {
+  cy.visit('http://localhost:4200/users')
+}
 
-When('I click the {string} button', (label) => {
-    cy.get('button').contains(label).click();
-  });
+Then("I'm in the delete confirmation page", () => {
+  cy.visit('http://localhost:4200/users/propagator/delete')
+}
 
-Then('I\'m logged in as user {string}', (username) => {
-  cy.get('#currentUser')
-    .invoke('text')
-    .should('contains', username);
-});
 
-Then('I see error message {string}', (message) => {
-  cy.get('.alert')
-    .invoke('text')
-    .should('contains', message);
-});
-
-Then('The {string} button is disabled', (label) => {
-  cy.get('button').contains(label)
-    .should('be.disabled');
-});
-
-Then('The {string} menu is not present', (option) => {
-  cy.get('.nav-link').contains(option)
-    .should('not.exist');
-});
-
-Then('I see input field feedback message {string}', (message) => {
-  cy.get('.invalid-feedback')
-    .should('be.visible')
-    .invoke('text')
-    .should('contains', message);
-});
