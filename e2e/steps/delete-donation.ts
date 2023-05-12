0d868295e13ef16bba6520a813aec760f72412f2
Oriol Agost Batalla <gpg18@agost.info>
2023-05-12 18:28:20 +0200

    fix: delete donation feature definition
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- e2e/steps/delete-donation.ts
+++ e2e/steps/delete-donation.ts
@@ -1,13 +1,13 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 import { DataTable } from '@cucumber/cucumber';
 
-When(/^I click the list button$/, function () {
+When(/^I click the donation list button$/, function () {
   cy.get('#listDonations').click();
 });
 When(/^I click on the first item$/, function () {
   cy.get('.table').first().click();
 });
-When(/^I click on the delete button$/, function () {
+When(/^I click on the donation delete button$/, function () {
   cy.get('button').contains('Delete').click();
 });
 When(/^I click on the confirm button$/, function () {
@@ -16,6 +16,6 @@ When(/^I click on the confirm button$/, function () {
 Then(/^I am redirected to the donation list page$/, function () {
   cy.url().should('match', /\/donations/);
 });
-Then(/^I should not see the delete button$/, function () {
+Then(/^I should not see the donation delete button$/, function () {
   cy.get('button').contains('Delete').should('not.exist');
 });
