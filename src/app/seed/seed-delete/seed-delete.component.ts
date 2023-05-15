3003025977774aba56c6551d1dfa90fa714fa24e
djuza <ilhanibrahimkadic@gmail.com>
2023-05-15 17:34:12 +0200

    feat(seed): add seed-delete ts

--- src/app/seed/seed-delete/seed-delete.component.ts
+++ src/app/seed/seed-delete/seed-delete.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SeedService } from '../seed.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-seed-delete',
   templateUrl: './seed-delete.component.html',
   styleUrls: ['./seed-delete.component.scss'],
 })
-export class SeedDeleteComponent {}
+export class SeedDeleteComponent implements OnInit {
+  idSeed: string;
+  constructor(
+    private router: Router,
+    private seedService: SeedService,
+    private route: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    this.idSeed = this.route.snapshot.paramMap.get('id');
+  }
+  delete() {
+    this.seedService.deleteResourceById(this.idSeed).subscribe({
+      next: (e) => {
+        console.log(e);
+        this.router.navigateByUrl('/seed');
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+}
