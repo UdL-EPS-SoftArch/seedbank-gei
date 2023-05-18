fbd25ebbf38cc67d84fc67189d2743da3c817585
sys-walker <pere.rollon@gmail.com>
2023-05-18 17:32:42 +0200

    Add Steps in create-take.ts
    Co-authored-by: sys-walker <pere.rollon@gmail.com>
    Co-authored-by: Oscar <oscar13785@gmail.com>
    Co-authored-by: MarioMontanuy <mariobnf58@gmail.com>

--- e2e/steps/create-take.ts
+++ e2e/steps/create-take.ts
@@ -21,3 +21,32 @@ Then('The {string} button is not present', (option) => {
   cy.get('.jumbotron').contains(option)
     .should('not.exist');
 });
+
+//  Steps for add take fro propagator,donor,admin
+
+Then("I'm in donations take list page", () => {
+  cy.visit("http://localhost:4200/donations");
+});
+
+And("I click on the first element list", () => {
+  cy.get("tr").first().click();
+  cy.url().should("match", /http:\/\/localhost:4200\/donations\/(\d+)/);
+});
+And("I check if add take button exists", () => {
+  cy.get("#addTakeBtn").should("exist");
+});
+
+And("I check if add take button not exists", () => {
+  cy.get("#addTakeBtn").should("not.exist");
+});
+
+Then("I click the button",()=>{
+  cy.get("#addTakeBtn").should("exist");
+  cy.get("#addTakeBtn").click();
+  cy.url().should("be.equals", "http://localhost:4200/take/add");
+})
+Then("I check if were autofilled",()=>{
+  cy.get("#amount").should('not.have.value', '');
+  cy.get("#weight").should('not.have.value', '');
+  cy.get("#location").should('not.have.value', '');
+})
