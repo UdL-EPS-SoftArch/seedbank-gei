9388e634aee944f39452c1e6abb64630e358b6b8
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2023-05-24 11:56:18 +0200

    feat: add selector for seeds
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/app/donation/donation-create/donation-create.component.ts
+++ src/app/donation/donation-create/donation-create.component.ts
@@ -4,6 +4,9 @@ import {Donation} from "../donation";
 import {DonationService} from "../donation.service";
 import {Location} from "@angular/common";
 import {AuthenticationBasicService} from "../../login-basic/authentication-basic.service";
+import {SeedService} from "../../seed/seed.service";
+import {Seed} from "../../seed/seed";
+import {PagedResourceCollection} from "@lagoshny/ngx-hateoas-client";
 
 @Component({
   selector: 'app-donation-create',
@@ -12,17 +15,20 @@ import {AuthenticationBasicService} from "../../login-basic/authentication-basic
 })
 export class DonationCreateComponent implements OnInit {
   public donation: Donation;
+  public seeds: Seed[] = [];
 
   constructor(
     private router: Router,
     private locationService: Location,
     private donationService: DonationService,
+    private seedService: SeedService,
     private authenticationBasicService: AuthenticationBasicService,
   ) {
   }
 
   ngOnInit(): void {
     this.donation = new Donation();
+    this.loadSeedList();
   }
 
   onSubmit(): void {
@@ -32,6 +38,15 @@ export class DonationCreateComponent implements OnInit {
       this.router.navigate([uri]).then();
     });
   }
+
+  loadSeedList() {
+    this.seedService.getPage({
+      sort: {scientificName: 'ASC'},
+    }).subscribe((seeds: PagedResourceCollection<Seed>) => {
+      this.seeds = seeds.resources
+    })
+  }
+
 
   onCancel(): void {
     this.locationService.back();
