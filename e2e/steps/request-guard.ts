d21d5eadd9197538eff17673dd7677aeddcfed44
FerranAD <ferranaran@tuta.io>
2023-06-01 16:30:54 +0200

    feat(donation): implement e2e for remove and edit
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- e2e/steps/request-guard.ts
+++ e2e/steps/request-guard.ts
@@ -2,3 +2,10 @@ import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 When(/^I go to the create a new request url$/, function () {
   cy.visit('http://localhost:4200/requests/create');
 });
+When(/^I go to the edit a request url$/, function () {
+  cy.visit('http://localhost:4200/requests/1/edit');
+});
+
+When(/^I go to the remove a request url$/, function () {
+  cy.visit('http://localhost:4200/requests/1/delete');
+});
