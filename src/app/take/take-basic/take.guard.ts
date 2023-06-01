0ae0b7f6a7ae41058d777b93e8608d241ef55d6c
Oriol Agost Batalla <gpg18@agost.info>
2023-06-01 15:32:23 +0200

    refactor: take guard now uses `joinPath` from shared file
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/app/take/take-basic/take.guard.ts
+++ src/app/take/take-basic/take.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlSegment, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthenticationBasicService} from "../../login-basic/authentication-basic.service";
+import {joinPath} from "../../guards/join-path";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class TakeGuard {
   canActivate(
     route: ActivatedRouteSnapshot) {
     let path = "";
-    route.url[1] ? path = this.joinPath(route.url) : path = route.url[0].path;
+    route.url[1] ? path = joinPath(route.url) : path = route.url[0].path;
     console.log(path)
     if(path.includes("edit") || path.includes("delete")) {
       console.log("editdelete")
@@ -32,13 +33,5 @@ export class TakeGuard {
     return true;
   }
 
-  private joinPath(url: UrlSegment[]) {
-    let finalPath: string = "";
-    url.forEach(segment => {
-      finalPath += segment.path + "/";
-    });
-    finalPath = finalPath.substring(0, finalPath.length - 1);
-    return finalPath;
-  }
 
 }
