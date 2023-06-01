49db852b590cf1c51235587b0d53212873aca6f6
FerranAD <ferranaran@tuta.io>
2023-06-01 16:27:25 +0200

    feat(donations): implement create request guard
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- e2e/steps/request-guard.ts
+++ e2e/steps/request-guard.ts
@@ -1,1 +1,4 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+When(/^I go to the create a new request url$/, function () {
+  cy.visit('http://localhost:4200/requests/create');
+});
