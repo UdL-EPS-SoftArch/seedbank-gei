dde2c0a12a3ca29af218d7915d49aeb932cbb3aa
Oriol Agost Batalla <gpg18@agost.info>
2023-06-01 15:03:43 +0200

    feat: gets path in donation guard
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/app/donation/donation-guard/donation.guard.ts
+++ src/app/donation/donation-guard/donation.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlSegment, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -9,7 +9,18 @@ export class DonationGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+    let path: string = route.url ? this.joinPath(route.url) : route.url[0].path;
+    console.log(path)
+    return true
   }
 
+
+  private joinPath(url: UrlSegment[]) {
+    let finalPath: string = "";
+    url.forEach((segment: UrlSegment) => {
+      finalPath += segment.path + "/";
+    });
+    return finalPath.substring(0, finalPath.length - 1);
+  }
 }
