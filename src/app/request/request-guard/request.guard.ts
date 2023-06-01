ccc20514ba810b8392de942cfb8a10e151a15b65
Oriol Agost Batalla <gpg18@agost.info>
2023-06-01 16:05:04 +0200

    feat: adds authentication service as constructor on request guard
    Co-authored-by: oriolagobat <oriolagobat@gmail.com>
    Co-authored-by: FerranAD <farandomingo@gmail.com>
    Co-authored-by: Pablito2020 <pablofrailealonso@gmail.com>

--- src/app/request/request-guard/request.guard.ts
+++ src/app/request/request-guard/request.guard.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
 import { Observable } from 'rxjs';
+import {AuthenticationBasicService} from "../../login-basic/authentication-basic.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestGuard implements CanActivate {
+  constructor(private authenticationService: AuthenticationBasicService) {
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
