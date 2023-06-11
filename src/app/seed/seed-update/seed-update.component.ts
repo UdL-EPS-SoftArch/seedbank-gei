6167307e4ee76184e868f479159e7ea8d1b3bf6b
adnan.bubalo <bubalo.adnan@gmail.com>
2023-06-11 22:32:12 +0200

    feat(seed): add edit beneficial seeds on seed edit page

--- src/app/seed/seed-update/seed-update.component.ts
+++ src/app/seed/seed-update/seed-update.component.ts
@@ -3,6 +3,8 @@ import { Seed } from '../seed';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SeedService } from '../seed.service';
 import { PagedResourceCollection } from '@lagoshny/ngx-hateoas-client';
+import { FormGroup } from '@angular/forms';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-seed-update',
@@ -10,14 +12,22 @@ import { PagedResourceCollection } from '@lagoshny/ngx-hateoas-client';
   styleUrls: ['./seed-update.component.css'],
 })
 export class SeedUpdateComponent {
+  closeResult = '';
+  public isModalSaved: boolean = false;
   public seed: Seed = new Seed();
+  public seeds: Seed[] = [];
   public commonNameInput: string = '';
   public commonNamesList: any = [];
-
+  public beneficialForList: any = [];
+  public selectedSeed: String | undefined = undefined;
+  public showModal: boolean = false;
+  public seedForm: FormGroup;
+  public beneficialSeeds: any = [];
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private seedService: SeedService
+    private seedService: SeedService,
+    private modalService: NgbModal
   ) {}
 
   ngOnInit(): void {
@@ -25,7 +35,77 @@ export class SeedUpdateComponent {
     this.seedService.getResource(id).subscribe((seed: Seed) => {
       this.seed = seed;
       this.commonNamesList = this.seed.commonName;
+      this.loadSeedList();
+      this.loadBeneficialSeeds(id);
     });
+  }
+
+  loadSeedList() {
+    this.seedService
+      .getPage({
+        sort: { scientificName: 'ASC' },
+      })
+      .subscribe((seeds: PagedResourceCollection<Seed>) => {
+        this.seeds = seeds.resources.sort((a, b) =>
+          a.scientificName.localeCompare(b.scientificName)
+        );
+        this.beneficialSeeds = this.seeds.map((obj) => ({
+          ...obj,
+          isChecked: false,
+        }));
+      });
+  }
+
+  loadBeneficialSeeds(id: string) {
+    this.seedService.getBeneficialFor(id).subscribe((response) => {
+      this.seed.beneficialFor = response._embedded.seeds;
+      this.beneficialSeeds.forEach((seed1) => {
+        const matchingObj = this.seed.beneficialFor.find(
+          (seed2) => seed2.uri === seed1.uri
+        );
+        if (matchingObj) {
+          seed1.isChecked = true;
+        }
+      });
+    });
+  }
+
+  open(content) {
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  onChangeBeneficialFor(seed) {
+    if (this.beneficialForList.includes(seed)) {
+      this.beneficialForList = this.beneficialForList.filter(
+        (item) => item !== seed
+      );
+    } else {
+      this.beneficialForList.push(seed);
+    }
+  }
+
+  saveAndClose(modal: any) {
+    this.isModalSaved = true;
+    modal.close('Save click');
   }
 
   addCommonName(commonNameInput: string) {
@@ -39,7 +119,9 @@ export class SeedUpdateComponent {
 
   onSubmit(): void {
     this.seed.commonName = this.commonNamesList;
-    this.seed.beneficialFor = [];
+    this.seed.beneficialFor = this.beneficialForList.sort((a, b) =>
+      a.scientificName.localeCompare(b.scientificName)
+    );
     this.seedService.patchResource(this.seed).subscribe((patchedSeed: Seed) => {
       this.router.navigate(['seeds', patchedSeed.id]);
     });
